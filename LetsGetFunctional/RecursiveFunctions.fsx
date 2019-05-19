054e6089100e9223f9a263cedc026b0b5d7bef8b
Luka Oghadze <loghadze@justice.gov.ge>
2019-05-19 16:35:54 +0400

    added Recursive Functions Examples

---
+++ LetsGetFunctional/RecursiveFunctions.fsx
@@ -0,0 +1,12 @@
+﻿// Non Tail Call Version
+let rec factorial1 v =
+    match v with | 1L -> 1L
+                 | _ -> v * factorial1 (v - 1L)
+
+
+// Tail Call Version
+let factorial2 v =
+    let rec fact c p =
+        match c with | 0L -> p
+                     | _ -> fact (c - 1L) (c * p)
+    fact v 1L

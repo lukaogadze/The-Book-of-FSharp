a3ea9b1a6f5f7c0372695ed93b0b0791deeb226a
Luka Oghadze <loghadze@justice.gov.ge>
2019-05-19 16:26:12 +0400

    removed code duplication

--- LetsGetFunctional/FunctionComposition.fsx
+++ LetsGetFunctional/FunctionComposition.fsx
@@ -1,14 +1,8 @@
-﻿let fahrenheitToCelsius degreeesF = (degreeesF - 30.0) * (5.0 / 9.0)
+﻿#load "shared.fsx";
 
-let marchHighTemps = [ 33.0; 30.0; 33.0; 38.0; 36.0; 31.0; 35.0;
-                       42.0; 53.0; 65.0; 59.0; 42.0; 31.0; 41.0;
-                       49.0; 45.0; 37.0; 42.0; 40.0; 32.0; 33.0;
-                       42.0; 48.0; 36.0; 34.0; 38.0; 41.0; 46.0;
-                       54.0; 57.0; 59.0 ]
+let averageInCelsius = List.average >> Shared.fahrenheitToCelsius
 
-let averageInCelsius = List.average >> fahrenheitToCelsius
-
-printfn "March average (C): %.1f" <| averageInCelsius marchHighTemps
+printfn "March average (C): %.1f" <| averageInCelsius Shared.marchHighTemps
 
 let delay = System.TimeSpan.FromSeconds >> System.Threading.Thread.Sleep
 delay 5.0
--- LetsGetFunctional/Pipelining.fsx
+++ LetsGetFunctional/Pipelining.fsx
@@ -1,27 +1,16 @@
-﻿let add a =
-    let innerAdd b =
-        a + b
-    innerAdd
+﻿#load "shared.fsx"
 
 // Forward Pipelining
-add 2 3 |> ignore
+Shared.add 2 3 |> ignore
 
-let fahrenheitToCelsius degreeesF = (degreeesF - 30.0) * (5.0 / 9.0)
-
-let marchHighTemps = [ 33.0; 30.0; 33.0; 38.0; 36.0; 31.0; 35.0;
-                       42.0; 53.0; 65.0; 59.0; 42.0; 31.0; 41.0;
-                       49.0; 45.0; 37.0; 42.0; 40.0; 32.0; 33.0;
-                       42.0; 48.0; 36.0; 34.0; 38.0; 41.0; 46.0;
-                       54.0; 57.0; 59.0 ]
-
-marchHighTemps
+Shared.marchHighTemps
 |> List.average
-|> fahrenheitToCelsius
+|> Shared.fahrenheitToCelsius
 |> printfn "March Average (C): %.1f"
 
 // Backward Pipelining
 
-printfn "March Average (F): %.1f" <| List.average marchHighTemps
+printfn "March Average (F): %.1f" <| List.average Shared.marchHighTemps
 
 let addStuff a b c = a + b + c
 

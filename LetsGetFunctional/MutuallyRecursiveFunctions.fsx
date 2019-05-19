6b568a7ba071a812c5c2ed33b899e2e31de06bdc
Luka Oghadze <loghadze@justice.gov.ge>
2019-05-19 16:42:58 +0400

    added Mutually Recursive Functions Examples

---
+++ LetsGetFunctional/MutuallyRecursiveFunctions.fsx
@@ -0,0 +1,8 @@
+﻿let fibonacci n =
+    let rec f = function
+                | 1 -> 1
+                | n -> g (n - 1)
+    and g = function
+            | 1 -> 0
+            | n -> g (n - 1) + f(n - 1)
+    f n + g n

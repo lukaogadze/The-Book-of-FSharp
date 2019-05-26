4fb9dc7cf5c6cc6256d37444f88d31f215f672c8
holo <holo@HOLO-PC>
2019-05-26 21:24:32 +0400

    added LazyEvaluation

---
+++ LetsGetFunctional/LazyEvaluation.fsx
@@ -0,0 +1,13 @@
+﻿let lazyOperation = lazy(printfn "evaluating lazy expression"
+                         System.Threading.Thread.Sleep 3000
+                         42)
+
+
+#time
+lazyOperation.Force() |> printfn "Result: %d"
+#time
+
+#time
+lazyOperation.Force() |> printfn "Result: %d"
+#time
+

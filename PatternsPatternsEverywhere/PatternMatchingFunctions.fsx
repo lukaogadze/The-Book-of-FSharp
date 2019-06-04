1240988fce5943aecc2f098e5d4bb04402a284c9
holo <holo@HOLO-PC>
2019-06-05 02:26:16 +0400

    added PatternMatchingFunctions

--- PatternsPatternsEverywhere/PatternMatchingFunctions.fsx
+++ PatternsPatternsEverywhere/PatternMatchingFunctions.fsx
@@ -129,3 +129,53 @@ let x2, y2 = point1
 
 let x3, y3 as point2 = 10, 20
 
+
+let locatePoint1 = function
+    | (0, 0) as p -> sprintf "%A is at the origin" p
+    | (x, y) & (_, 0) -> sprintf "(%i, %i) is on the x-axis" x y
+    | (x, y) & (0, _) -> sprintf "(%i, %i) is on the y-axis" x y
+    | (x, y) -> sprintf "Point (%i, %i)" x y
+
+let locatePoint2 = function
+    | (0, 0) as p -> sprintf "%A is at the origin" p
+    | (_, 0) | (0, _) as p -> sprintf "%A is on an axis" p
+    | p -> sprintf "Point %A" p
+
+let locatePoint3 = function
+    | (0, 0) as p -> sprintf "%A is at the origin" p
+    | (x, y) & ((_, 0) | (0, _)) -> sprintf "(%i, %i) is on an axis" x y
+    | p -> sprintf "Point %A" p
+
+let (|Fizz|Buzz|FizzBuzz|Other|) n =
+    match (n % 3, n % 5) with
+    | 0, 0 -> FizzBuzz
+    | 0, _ -> Fizz
+    | _, 0 -> Buzz
+    | _ -> Other n
+
+let fizzBuzz = function
+    | Fizz -> "Fizz"
+    | Buzz -> "Buzz"
+    | FizzBuzz -> "FizzBuzz"
+    | Other n -> n.ToString()
+
+seq {1..100}
+|> Seq.map fizzBuzz
+|> Seq.iter (printfn "%s")
+
+let (|Fizz|_|) n = if n % 3 = 0 then Some Fizz else None
+let (|Buzz|_|) n = if n % 5 = 0 then Some Buzz else None
+
+let fizzBuzz2 = function
+    | Fizz & Buzz -> "FizzBuzz"
+    | Fizz -> "Fizz"
+    | Buzz -> "Buzz"
+    | n -> n.ToString()
+
+let (|DivisibleBy|_|) d n = if n % d = 0 then Some DivisibleBy else None
+
+let fizzBuzz3 = function
+    | DivisibleBy 3 & DivisibleBy 5 -> "FizzBuzz"
+    | DivisibleBy 3 -> "Fizz"
+    | DivisibleBy 5 -> "Buzz"
+    | n -> n.ToString()

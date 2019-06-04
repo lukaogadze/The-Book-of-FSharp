da4cc76b989d67d02da9b9042319ca962f005e06
holo <holo@HOLO-PC>
2019-06-05 02:28:34 +0400

    added MeasuringUp Project

---
+++ MeasuringUp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeasuringUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
